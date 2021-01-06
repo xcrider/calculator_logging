570a2b8103f0b913ef940d60a9f04c64d21e6e32
Karol Zawada <k.zawadaa@gmail.com>
2021-01-06 14:45:01 +0100

    Create calculator functions

---
+++ main.py
@@ -0,0 +1,44 @@
+
+def addition():
+    a = int(input("Podaj skladnik 1: "))
+    b = int(input("Podaj skladnik 2: "))
+    result = a + b
+    print (result)
+def subtraction():
+  
+    a = int(input("Podaj skladnik 1: "))
+    b = int(input("Podaj skladnik 2: "))
+    result = a - b
+    print (result)
+
+def multiplication():
+    
+    a = int(input("Podaj skladnik 1: "))
+    b = int(input("Podaj skladnik 2: "))
+    result = a * b 
+    print (result)
+
+def division():
+    
+    a = int(input("Podaj skladnik 1: "))
+    b = int(input("Podaj skladnik 2: "))
+    result = a / b 
+    print (result)
+
+if __name__ == "__main__":
+
+    calculation_type = int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "))
+
+    if calculation_type == 1:
+        addition()
+
+    elif calculation_type == 2:
+        subtraction()
+    
+    elif calculation_type == 3:
+        multiplication()
+
+    elif calculation_type == 4:
+        division()
+    elif calculation_type > 4:
+        print("Nie umiem wykonać tej operacji")
